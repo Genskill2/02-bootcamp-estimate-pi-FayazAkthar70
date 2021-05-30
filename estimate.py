6bb0bd0d0ca91cd31ff5b2a074a33b2c9c2cbaf3
Fayaz Akthar <fayaz_b190481cs@nitc.ac.in>
2021-05-30 15:49:42 +0530

    first draft of monte_carlo func

--- estimate.py
+++ estimate.py
@@ -1,13 +1,29 @@
 import math
 import unittest
+import random
 
 class TestWallis(unittest.TestCase):
+
     def wallis(iteration_num):
-        pi_estimate = 1
+        pi_estimate = 1.0
         for i in range(1,iteration_num+1):
             est_for_ith_iteration = (4*i*i) / ((4*i*i) - 1)
             pi_estimate *= est_for_ith_iteration 
+        pi_estimate *= 2
         return pi_estimate
+    
+    def monte_carlo(iteration_num):
+        pi_estimate,circle_area,square_area = 0
+        for i in range(iteration_num):
+            x = random.random()
+            y = random.random()
+            is_inside = 1 <= pow(pow(x,2) + pow(x,2),0.5)
+            if(is_inside):
+                circle_area += 1
+            square_area += 1
+        pi_estimate = (4 * circle_area) / square_area 
+        return pi_estimate
+
 
     def test_low_iters(self):
         for i in range(0, 5):
